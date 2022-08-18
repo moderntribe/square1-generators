70fd840d5da91503ceb207c83f8b78b367ef9a52
Alexander Kozhukhovski <aliaksandr.kazhukhouski@gmail.com>
2022-08-18 18:53:55 +0200

    Fixes CS issues for post_type generator

--- templates/post_type/config.php
+++ templates/post_type/config.php
@@ -1,16 +1,18 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Tribe\Project\Post_Types\%1$s;
 
 use Tribe\Libs\Post_Type\Post_Type_Config;
 
 class Config extends Post_Type_Config {
+
+	// phpcs:ignore SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingAnyTypeHint
 	protected $post_type = %1$s::NAME;
 
-	public function get_args() {
+	public function get_args(): array {
 		return [
 			'hierarchical'     => false,
-			'enter_title_here' => __( '%2$s title', 'tribe' ),
+			'enter_title_here' => esc_html__( '%2$s title', 'tribe' ),
 			'menu_icon'        => 'dashicons-warning',
 			'map_meta_cap'     => true,
 			'supports'         => [ 'title', 'editor', 'author', 'thumbnail', 'page-attributes', 'excerpt', 'revisions', ],
@@ -19,11 +21,14 @@ class Config extends Post_Type_Config {
 		];
 	}
 
-	public function get_labels() {
+	/**
+	 * @return array<string, string>
+	 */
+	public function get_labels(): array {
 		return [
-			'singular' => __( '%2$s', 'tribe' ),
-			'plural'   => __( '%3$s', 'tribe' ),
-			'slug'     => __( '%4$s', 'tribe' ),
+			'singular' => esc_html__( '%2$s', 'tribe' ),
+			'plural'   => esc_html__( '%3$s', 'tribe' ),
+			'slug'     => '%4$s',
 		];
 	}
 
--- templates/post_type/subscriber.php
+++ templates/post_type/subscriber.php
@@ -1,10 +1,12 @@
-<?php
-declare( strict_types=1 );
+<?php declare(strict_types=1);
 
 namespace Tribe\Project\Post_Types\%1$s;
 
 use Tribe\Libs\Post_Type\Post_Type_Subscriber;
 
 class Subscriber extends Post_Type_Subscriber {
+
+	// phpcs:ignore SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingAnyTypeHint
 	protected $config_class = Config::class;
+
 }
